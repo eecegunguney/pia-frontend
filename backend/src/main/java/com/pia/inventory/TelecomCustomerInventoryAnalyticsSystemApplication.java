939d2e1b09b7bfea5498cee22dc92eb02734a562
seymaa54 <nalbantseymaa@gmail.com>
2026-07-05 14:31:18 +0300

    initialize project structure with Maven and Spring Boot setup

---
+++ backend/src/main/java/com/pia/inventory/TelecomCustomerInventoryAnalyticsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pia.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomCustomerInventoryAnalyticsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomCustomerInventoryAnalyticsSystemApplication.class, args);
+	}
+
+}
